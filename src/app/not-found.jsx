81d83f16cc29f50619b1458c3b7790b9bc354767
bgb10 <bgb10@naver.com>
2023-11-01 22:19:57 +0900

    chore: set up environment for development
    remove unused files and set typescript and install additional packages

--- src/app/not-found.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="prose max-w-none pt-10 pb-8">
-      <h2>Page Not Found</h2>
-      <p>We couldn&apos;t locate the page you&apos;re looking for.</p>
-    </div>
-  )
-}
