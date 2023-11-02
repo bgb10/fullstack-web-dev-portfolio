91463b2a35bf43e6bdbe34ec5a0bc0187ab0f5ae
bgb10 <bgb10@naver.com>
2023-11-02 23:20:57 +0900

    feat: add ProjectListItem

---
+++ src/components/organisms/project/ProjectListItem.tsx
@@ -0,0 +1,15 @@
+import ResumeListItem from '@components/organisms/ResumeListItem'
+import { ProjectType } from '@components/types'
+
+const ProjectListItem: React.FC<ProjectType> = ({
+  name,
+  description,
+  details,
+  skills
+}) => (
+  <ResumeListItem title={name} label={description} details={details}>
+    {/* Here tags to be added! */}
+  </ResumeListItem>
+)
+
+export default ProjectListItem
