c96395f7c0cd49140ad554e1f92ec84da483f3c2
bgb10 <bgb10@naver.com>
2023-11-02 22:22:34 +0900

    feat: add EducationLayout template

---
+++ src/components/templates/EducationLayout.tsx
@@ -0,0 +1,18 @@
+import { EducationType, ExperienceType } from '@components/types'
+import Heading from '@components/molecules/Heading'
+import EducationListItem from '@components/organisms/education/EducationListItem'
+
+type EducationProps = {
+  items: EducationType[]
+}
+
+const EducationLayout = (props: EducationProps) => (
+  <>
+    <Heading title="Education" description="share my education yeah!" />
+    {props.items.map((item, index) => (
+      <EducationListItem key={index} {...item} />
+    ))}
+  </>
+)
+
+export default EducationLayout
