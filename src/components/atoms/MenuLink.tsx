83fc21aae20d357162b8c8f48293ce6783c05dee
bgb10 <bgb10@naver.com>
2023-11-02 14:52:17 +0900

    feat: add MenuLink component to show each menu link

---
+++ src/components/atoms/MenuLink.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const MenuLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 18px;
+  color: #000000;
+  text-decoration: none;
+`
+
+export default MenuLink
