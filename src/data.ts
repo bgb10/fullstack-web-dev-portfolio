5d4d4604f562d6417a586ae7f288fc000b262a15
bgb10 <bgb10@naver.com>
2023-11-02 22:04:12 +0900

    feat: add dummy data for Experience page

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+import { ExperienceType } from '@components/types'
+
+export const experienceList: ExperienceType[] = [
+  {
+    company: 'Republic of Korea Air Force(ROKAF)',
+    job: 'Software Engineer',
+    duration: {
+      startedAt: new Date(2000, 1),
+      endedAt: new Date(2000, 10),
+      isCompleted: true
+    },
+    details:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+  },
+  {
+    company: 'Republic of Korea Air Force(ROKAF)',
+    job: 'Software Engineer',
+    duration: {
+      startedAt: new Date(2000, 1),
+      endedAt: new Date(2000, 10),
+      isCompleted: true
+    },
+    details:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+  }
+]
