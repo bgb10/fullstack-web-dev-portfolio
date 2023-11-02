2fb4afd82cf8b842dc08370c71cb92dc717b5ef8
bgb10 <bgb10@naver.com>
2023-11-02 23:21:06 +0900

    feat: add ProjectLayout template

---
+++ src/components/templates/ProjectLayout.tsx
@@ -0,0 +1,18 @@
+import { ProjectType } from '@components/types'
+import Heading from '@components/molecules/Heading'
+import ProjectListItem from '@components/organisms/project/ProjectListItem'
+
+type ProjectProps = {
+  items: ProjectType[]
+}
+
+const ProjectLayout = (props: ProjectProps) => (
+  <>
+    <Heading title="Project" description="share my project. see this yeah!" />
+    {props.items.map((item, index) => (
+      <ProjectListItem key={index} {...item} />
+    ))}
+  </>
+)
+
+export default ProjectLayout
