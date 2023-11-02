5288875d75876396be0f0b4d257c8a71da34615f
bgb10 <bgb10@naver.com>
2023-11-02 22:22:24 +0900

    feat: add EducationListItem

---
+++ src/components/organisms/education/EducationListItem.tsx
@@ -0,0 +1,31 @@
+import ResumeListItem from '@components/organisms/ResumeListItem'
+import { EducationType } from '@components/types'
+
+const createEducationTitle = (institution: string, country: string) =>
+  institution + ', ' + country
+
+const EducationListItem: React.FC<EducationType> = ({
+  institution,
+  country,
+  isDegreeProgram,
+  degree,
+  duration,
+  details
+}) => {
+  // Create degree text.
+  const _degree = () =>
+    `Degree: ${duration.isCompleted ? '' : 'Pursuing'} ${
+      degree?.name
+    }'s degree in ${degree?.major}`
+
+  return (
+    <ResumeListItem
+      title={createEducationTitle(institution, country)}
+      description={isDegreeProgram ? _degree() + ' | ' + degree?.gpa : ''}
+      duration={duration}
+      details={details}
+    />
+  )
+}
+
+export default EducationListItem
