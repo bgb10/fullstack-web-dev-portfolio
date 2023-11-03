f82b36ce75bef59cdc89c42f6529aed21a223436
bgb10 <bgb10@naver.com>
2023-11-04 00:20:01 +0900

    feat: add TimeLine in Experience

--- src/components/templates/ExperienceLayout.tsx
+++ src/components/templates/ExperienceLayout.tsx
@@ -1,18 +1,33 @@
 import { ExperienceType } from 'src/types'
 import ExperienceListItem from '@components/organisms/experience/ExperienceListItem'
 import Heading from '@components/molecules/Heading'
+import TimeLine, { TimeLineElements } from '@components/molecules/TimeLine'
 
 type ExperienceProps = {
   items: ExperienceType[]
 }
 
-const ExperienceLayout = (props: ExperienceProps) => (
-  <>
-    <Heading title="Experience" description="Where, When and How I've worked" />
-    {props.items.map((item, index) => (
-      <ExperienceListItem key={index} {...item} />
-    ))}
-  </>
-)
+const ExperienceLayout = (props: ExperienceProps) => {
+  const experiences: TimeLineElements[] = props.items.map((item) => {
+    return {
+      name: item.company,
+      startedAt: item.duration.startedAt,
+      endedAt: item.duration.endedAt
+    }
+  })
+
+  return (
+    <>
+      <Heading
+        title="Experience"
+        description="Where, When and How I've worked"
+      />
+      <TimeLine name="Experience" elements={experiences}></TimeLine>
+      {props.items.map((item, index) => (
+        <ExperienceListItem key={index} {...item} />
+      ))}
+    </>
+  )
+}
 
 export default ExperienceLayout
