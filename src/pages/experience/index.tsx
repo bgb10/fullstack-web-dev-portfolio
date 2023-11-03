5563735e3698b3fcab89752b56710c58a7fe2c21
bgb10 <bgb10@naver.com>
2023-11-03 14:33:34 +0900

    feat: use query instead of local data to receive from db

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import ExperienceLayout from '@components/templates/ExperienceLayout'
-import { experienceList } from 'src/data'
+import { gql, useQuery } from '@apollo/client'
+import stringToDateConverter from 'src/utils/stringToDateConverter'
+
+const query = gql`
+  query Experiences {
+    experiences {
+      company
+      job
+      details {
+        raw
+      }
+      duration {
+        ... on Duration {
+          isCompleted
+          startedAt
+          endedAt
+        }
+      }
+    }
+  }
+`
 
 export default function Experience() {
+  const { loading, error, data } = useQuery(query)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error : {error.message}</p>
+
+  const convertedData = stringToDateConverter(data)
+
   return (
     <>
-      <ExperienceLayout items={experienceList}></ExperienceLayout>
+      <ExperienceLayout items={convertedData['experiences']}></ExperienceLayout>
     </>
   )
 }
