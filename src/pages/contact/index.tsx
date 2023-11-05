73325b1dd30a6a1dfe548bd63eb951672c1eae5e
bgb10 <bgb10@naver.com>
2023-11-05 20:54:10 +0900

    feat: add contact page

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Heading from '@components/molecules/Heading'
+
+export default function Project() {
+  return (
+    <>
+      <Heading
+        title="Feel free to contact me."
+        description="Email me on bgb1028@gmail.com. Github, LinkedIn, and Instagram Links are also available in below."
+      ></Heading>
+    </>
+  )
+}
